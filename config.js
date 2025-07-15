f664675e7c585289ba8829f7602070fcc598002d
WHATSAPP-NEW-MD <whatsappmdnew@gmail.com>
2025-07-15 11:27:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SHANUWA-MD=W0BWDZha#2s_sBhQ6STVKAu45W5jnP7LB5HouMI8mHWTr9mP_BSU",
+SESSION_ID: process.env.SESSION_ID || "SHANUWA-MD=ti83QITT#bG1mvWxlmaUjnpaAFu1pkllJx7dFVhALG8KpmmaZetg",
 MONGODB: process.env.MONGODB || "mongodb+srv://lakshan54:Shadow123456@cluster0.4bwik.mongodb.net/",
 PREFIX:  process.env.PREFIX || ['.'] ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
